259c90b56dc3ab369ca6b0625f95e15d82646e1d
Nick Canales <nickcharlescanales@gmail.com>
2020-07-23 22:58:45 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+  super(props);
+  this.state = { apiResponse: "" };
+}
+
+callAPI() {
+  fetch("http://localhost:4000")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+}
+
+componentDidMount() {
+  this.callAPI();
+};
+
+
+render() {
   return (
     <div className="App">
-      <nav>my nav bar
-
-      </nav>
-      hello world
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1 className="App-title">React is super</h1>
+      </header>
+      <p className="App-intro">{this.state.apiResponse}</p>
     </div>
-  );
+)
+  
+}
 }
 
 export default App;
